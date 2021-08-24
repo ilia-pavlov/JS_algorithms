7621200e2054a2167cca5c45a3c62b294160acb8
ilia Pavlov <3472204455ilia@gmail.com>
2021-08-24 01:20:48 -0400

    arrIssue

--- algorithms/binarSearch.js
+++ algorithms/binarSearch.js
@@ -5,9 +5,9 @@ console.log(end + ' first look');
     while (start <= end) {
         let middle = Math.floor((start + end) / 2);
 console.log(middle + ' I"m pretty middle');        
-        if (sortedArray[middle] === key) {
-            // found the key
-            return middle;  // ?? why with longArray it's not stoping on 1 and keep going.
+        if (sortedArray[middle] === key) {  //?? since we using advance Array, it's probalby doesn't work!! sortedArray[middle]
+console.log('sortedArray[middle] = ' + sortedArray[middle])        // found the key
+            return middle;  
         } else if (sortedArray[middle] < key) {
             // continue searching to the right
             start = middle + 1;
@@ -22,7 +22,7 @@ console.log(end, middle + ' -1 this is the end');
     return -1;
 }
 
-let exclusive = 1024;
+let exclusive = 102400;
 let longArray = [...Array(exclusive).keys()];
 //console.log(longArray)
 let key = 2;
@@ -31,7 +31,7 @@ let result = binarySearch(longArray, key);
 console.log('');
 console.log('printed ' + result + ' result');
 
-let simpleArray = [1,2,3,4,5,6,7,8,9,10];
+let simpleArray = [1,2,3,4,5,6,7,8,9,10,11,12];
 let num = 2;
 
 let sipmeCase = binarySearch(simpleArray, num);
--- algorithms/main.js
+++ algorithms/main.js
@@ -1,37 +1,40 @@
-const binarySearch = (list, item) => {
-    let low = 0
-    let high = list.length - 1
-  
-    while (low <= high) {
-      const mid = Math.floor((low + high) / 2)
-      const guess = list[mid]
-  
-      if (guess === item) {
-        return mid
-      }
-  
-      if (guess > item) {
-        high = mid - 1
-      } else {
-        low = mid + 1
-      }
+function binarySearch(sortedArray, key){
+    let start = 0;
+    let end = sortedArray.length - 1;
+console.log(end + ' first look');
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+console.log(middle + ' I"m pretty middle');        
+        if (sortedArray[middle] === key) {  //?? since we using advance Array, it's probalby doen't work!! sortedArray[middle]
+console.log('sortedArray[middle] = ' + sortedArray[middle])        // found the key
+            return middle;  // ?? why with longArray it's not stoping on 1 and keep going.
+        } else if (sortedArray[middle] < key) {
+            // continue searching to the right
+            start = middle + 1;
+console.log(start + ' this is the start');
+        } else {
+            // search searching to the left
+            end = middle - 1;
+console.log(end, middle + ' -1 this is the end');
+        }
     }
-  
-    return null //if not found
-  }
+	// key wasn't found
+    return -1;
+}
 
-  let exclusive = 1024;
-  let longArray = [...Array(exclusive).keys()];
-  //console.log(longArray)
-  let key = 2;
-  
-  let result = binarySearch(longArray, key);
-  console.log('');
-  console.log('printed ' + result + ' result');
-  
-  let simpleArray = [1,2,3,4,5,6,7,8,9,10];
-  let num = 2;
-  
-  let sipmeCase = binarySearch(simpleArray, num);
-  console.log('');
-  console.log('printed ' + sipmeCase + ' result')  
+let exclusive = 102400;
+let longArray = [...Array(exclusive).keys()];
+//console.log(longArray)
+let key = 2;
+
+let result = binarySearch(longArray, key);
+console.log('');
+console.log('printed ' + result + ' result');
+
+let simpleArray = [1,2,3,4,5,6,7,8,9,10,11,12];
+let num = 2;
+
+let sipmeCase = binarySearch(simpleArray, num);
+console.log('');
+console.log('printed ' + sipmeCase + ' result')
+
